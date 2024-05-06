56de01a877de92c8806de739648350d3a684dbfd
TheBigSixthEmpire <167994546+TheBigSixthEmpire@users.noreply.github.com>
2024-05-06 18:06:42 +0800

    ...

--- cssjs/a.js
+++ cssjs/a.js
@@ -1,8 +1,243 @@
-var x = 280;
-var y = 880;
+var floors={
+"one":`
+<svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1700 900" style="margin-top: 30px;">
+	<g>
+		<rect x="0" y="0" width="1700" height="900" fill="white" stroke="white" stroke-width="5"/>
+	</g>
+	<g>
+		<rect class="way1-1" id="way1-1" x="100" y="600" width="1500" height="200" fill="red" stroke="black" stroke-width="5"/>
+		<text x="900" y="700" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">走廊</text>
+	</g>
+	<g>
+		<rect class="stairs1-1" x="300" y="500" width="200" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="400" y="550" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">西侧楼梯</text>
+	</g>
+	<g>
+		<rect class="door1" x="0" y="600" width="100" height="200" fill="red" stroke="black" stroke-width="5"/>
+		<text x="50" y="700" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">西门</text>
+	</g>
+	<g>
+		<rect class="way1-2" x="1500" y="0" width="100" height="600" fill="red" stroke="black" stroke-width="5"/>
+		<text x="1550" y="325" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">东侧</text>
+		<text x="1550" y="375" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">走廊</text>
+	</g>
+	<g>
+		<rect class="wc1-1" x="1600" y="100" width="100" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="1650" y="125" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">女</text>
+		<text x="1650" y="175" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">厕所</text>
+	</g>
+	<g>
+		<rect class="water1" x="1600" y="200" width="100" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="1650" y="225" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">接水</text>
+		<text x="1650" y="275" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">池</text>
+	</g>
+	<g>
+		<rect class="wc1-2" x="1600" y="300" width="100" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="1650" y="325" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">男</text>
+		<text x="1650" y="375" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">厕所</text>
+	</g>
+	<g>
+		<rect class="way1-3" x="200" y="0" width="100" height="600" fill="red" stroke="black" stroke-width="5"/>
+		<text x="250" y="275" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">西侧</text>
+		<text x="250" y="325" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">走廊</text>
+	</g>
+	<g>
+		<rect class="yws" x="100" y="0" width="100" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="150" y="25" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">医务</text>
+		<text x="150" y="75" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">室</text>
+	</g>
+	<g>
+		<rect class="door2" x="700" y="800" width="400" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="900" y="850" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">南门</text>
+	</g>
+	<g>
+		<rect class="class" x="100" y="800" width="200" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="200" y="850" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">初一六班</text>
+	</g>
+	<rect id="a" x="280" y="880" width="20" height="20" fill="yellow" stroke="blue" stroke-width="2"/>
+`,
+"two":`
+<svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1600 900" style="margin-top: 30px;">
+	<g>
+		<rect x="0" y="0" width="1600" height="900" fill="white" stroke="white" stroke-width="5"/>
+	</g>
+	<g>
+		<rect class="way1-1" id="way1-1" x="0" y="600" width="1500" height="200" fill="red" stroke="black" stroke-width="5"/>
+		<text x="800" y="700" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">走廊</text>
+	</g>
+	<g>
+		<rect class="stairs1-1" x="200" y="500" width="200" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="300" y="550" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">西侧楼梯</text>
+	</g>
+	<g>
+		<rect class="way1-2" x="1400" y="0" width="100" height="600" fill="red" stroke="black" stroke-width="5"/>
+		<text x="1450" y="325" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">东侧</text>
+		<text x="1450" y="375" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">走廊</text>
+	</g>
+	<g>
+		<rect class="wc1-1" x="1500" y="100" width="100" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="1550" y="125" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">女</text>
+		<text x="1550" y="175" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">厕所</text>
+	</g>
+	<g>
+		<rect class="water1" x="1500" y="200" width="100" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="1550" y="225" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">接水</text>
+		<text x="1550" y="275" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">池</text>
+	</g>
+	<g>
+		<rect class="wc1-2" x="1500" y="300" width="100" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="1550" y="325" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">男</text>
+		<text x="1550" y="375" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">厕所</text>
+	</g>
+	<g>
+		<rect class="class" x="0" y="800" width="200" height="100" fill="red" stroke="black" stroke-width="5"/>
+		<text x="100" y="825" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">solar</text>
+		<text x="100" y="875" text-anchor="middle" alignment-baseline="middle" font-size="40" fill="black">办公室</text>
+	</g>
+	<rect id="a" x="380" y="500" width="20" height="20" fill="yellow" stroke="blue" stroke-width="2"/>
+`
+}
+// 读取名为a的cookie
+function cread(a) {
+    var nameEQ = a + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
 
+// 修改名为a的cookie内容为b
+function cwrite(a, b) {
+    var d = new Date();
+    d.setTime(d.getTime() + (365*24*60*60*1000)); // 设置过期时间为一年后
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = a + "=" + b + ";" + expires + ";path=/";
+}
+
+// 修改名为a的cookie内容为空
+function cremove(a) {
+    document.cookie = a + '=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;';
+}
+
+// 删除所有cookie为空
+function removeall() {
+    var cookies = document.cookie.split("; ");
+    for (var c = 0; c < cookies.length; c++) {
+        var d = window.location.hostname.split(".");
+        while (d.length > 0) {
+            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=/';
+            var p = location.pathname.split('/');
+            document.cookie = cookieBase + '; path=/';
+            while (p.length > 0) {
+                document.cookie = cookieBase + '; path=' + p.join('/');
+                p.pop();
+            }
+            d.shift();
+        }
+    }
+}
+var floor=cread("place");
+if(floor===null){
+	cwrite("place","one");
+	floor=cread("place");
+	cwrite("x", 280);
+	cwrite("y", 880);
+}
+function isNaNumber(a) {
+    return isNaN(a) || a == null;
+}
+//var x = 0;
+//var y = 0;
+var x = cread("x");
+var y = cread("y");
+console.log(x);
+console.log(y);
+if(floor=="one"){
+}else if(floor=="two"){
+}else{
+	cwrite("place", "one");
+	location.reload();
+}
+if(isNaNumber(x) || isNaNumber(y)){
+	x = 0;
+	y = 0;
+	if(floor=="one"){
+		x = 280;
+		y = 880;
+	}else if(floor=="two"){
+		x = 380;
+		y = 500;
+	}
+}
+console.log(x);
+console.log(y);
+const svgg=floors[floor];
+function deleting(){
+	removeall();
+	window.location.href = '/';
+}
+var floor2 = {
+"one":"一层",
+"two":"二层",
+"six":"六班"
+}
+function reload(){
+	var elements = document.querySelectorAll(".head");
+	elements.forEach(function(element) {
+		element.remove();
+	});
+	const role = cread("peo");
+	if (role === 'dandan') {
+		const headerHtml = svgg+`
+		<h3><p class="peo">你现在是:单单</p>
+		<p class="place">当前位置:` + floor2[floor] + `</p>
+		<a onclick="deleting()" class="href born">重生</a></h3>
+		`;
+		const newContent = document.createElement('div');
+		newContent.innerHTML = headerHtml;
+		newContent.className = "head";
+		document.body.insertBefore(newContent, document.body.firstChild);
+	} else if (role === 'chang') {
+		const headerHtml = svgg+`
+		<h3><p class="peo">你现在是:阿长</p>
+		<p class="place">当前位置:` + floor2[floor] + `</p>
+		<a onclick="deleting()" class="href born">重生</a></h3>
+		`;
+		const newContent = document.createElement('div');
+		newContent.innerHTML = headerHtml;
+		newContent.className = "head";
+		document.body.insertBefore(newContent, document.body.firstChild);
+	} else if (role === 'pen') {
+		const headerHtml = svgg+`
+		<h3><p class="peo">你现在是:钢盆</p>
+		<p class="place">当前位置:` + floor2[floor] + `</p>
+		<a onclick="deleting()" class="href born">重生</a></h3>
+		`;
+		const newContent = document.createElement('div');
+		newContent.innerHTML = headerHtml;
+		newContent.className = "head";
+		document.body.insertBefore(newContent, document.body.firstChild);
+	} else if (role === 'sheep') {
+		const headerHtml = svgg+`
+		<h3><p class="peo">你现在是:张绵羊</p>
+		<p class="place">当前位置:` + floor2[floor] + `</p>
+		<a onclick="deleting()" class="href born">重生</a></h3>
+		`;
+		const newContent = document.createElement('div');
+		newContent.innerHTML = headerHtml;
+		newContent.className = "head";
+		document.body.insertBefore(newContent, document.body.firstChild);
+	} else {
+		deleting();
+	}
+}
 // 更新坐标函数
 function updateCoordinates() {
+//	cwrite("x",x);
+//	cwrite("y",y);
 	document.getElementById('x').textContent = x;
 	document.getElementById('y').textContent = y;
 	document.getElementById('a').x.baseVal.value = x;
@@ -13,88 +248,169 @@ function updateCoordinates() {
 document.addEventListener('keydown', function(event) {
 	// 按下的按键
 	var keyPressed = event.key.toLowerCase();
-	var step = 20;
+	const step = 20;
 	// 根据按下的按键更新坐标
-	switch (keyPressed) {
-		case 'w':
-			if(y == 600 && (x < 100 || (x >= 500 && x < 1300) || x >= 1600)){
+	if(floor=="one"){
+		switch (keyPressed) {
+            case 'enter':
+            	if(y >= 500 && y < 600 && (x >= 300 && x < 500)){
+					if(floor=="one"){
+						x = 280;
+						y = 880;
+					}else if(floor=="two"){
+						x = 380;
+						y = 500;
+					}
+					cwrite("x", x);
+					cwrite("y", y);
+					updateCoordinates();
+		       		cwrite("place", "two"); 
+		       		floor="two";
+		        	location.reload();
+//					reload();
+            	}
+                break;
+			case 'w':
+				if(y == 600 && (x < 200 || (x >= 500 && x < 1500))){
+					break;
+				}
+				if(y == 500 && (x >= 300 && x < 500)){
+					break;
+				}
+				if((y == 100 || y == 200 || y == 300) && x >= 1600){
+					break;
+				}
+				if (y > 0) {
+				    y = parseInt(y) - parseInt(step);
+				}
 				break;
-			}
-			if((x >= 100 && x < 200) && (y===100||y===200||y===300||y===400||y===600)){
+			case 'a':
+				if(x == 100 && (y >= 800 || y < 100)){
+					break;
+				}
+				if(x == 200 && (y >= 100 && y < 600)){
+					break;
+				}
+				if(x == 700 && y >= 800){
+					break;
+				}
+				if(x == 1500 && y < 600){
+					break;
+				}
+				if (x > 0) {
+				    x = parseInt(x) - parseInt(step);
+				}
 				break;
-			}
-			if(y == 500 && ((x >= 300 && x < 500) || (x >= 1300 && x < 1500))){
+			case 's':
+				if(y == 100-step && (x >= 100 && x < 200)){
+					break;
+				}
+				if((y == 400-step || y == 300-step || y == 200-step) && x >= 1600){
+					break;
+				}
+				if(y == 800-step && (x < 100 || (x >= 300 && x <= 700-step) || (x >= 1100 && x < 1600))){
+					break;
+				}
+				if (y < 900-step) {
+				    y = parseInt(y) + parseInt(step);
+				}
 				break;
-			}
-			if((y == 100 || y == 200 || y == 300) && x >= 1600){
+			case 'd':
+				if(x == 300-step && (y < 500 || y >= 800)){
+					break;
+				}
+				if(x == 500-step && (y >= 500 && y < 600)){
+					break;
+				}
+				if(x == 1100-step && y >= 800){
+					break;
+				}
+				if(x == 1600-step && ((y >= 400 && y < 800) || y < 100)){
+					break;
+				}
+				if (x < 1700-step) {
+				    x = parseInt(y) + parseInt(step);
+				}
 				break;
-			}
-		    if (y > 0) {
-		        y -= step;
-		    }
-		    break;
-		case 'a':
-			if((x===100 || x===300 || x===500 || x===700 || x===1100 || x===1300 || x===1500) && y >= 800 && y < 900){
+			default:
+				// 如果按下的不是 WASD 键，则不做任何操作
+				return;
+		}
+	}else if(floor=="two"){
+		switch (keyPressed) {
+            case 'enter':
+            	if(y >= 500 && y < 600 && (x >= 300 && x < 500)){
+					if(floor=="one"){
+						x = 280;
+						y = 880;
+					}else if(floor=="two"){
+						x = 380;
+						y = 500;
+					}
+					cwrite("x", x);
+					cwrite("y", y);
+					updateCoordinates();
+		       		cwrite("place", "two"); 
+		       		floor="two";
+//		        	location.reload();
+					reload();
+            	}
+                break;
+			case 'w':
+				if(y == 500 && (x >= 200 && x < 400)){
+					break;
+				}
+				if(y == 600 && (x < 200 || (x >= 400 && x < 1400))){
+					break;
+				}
+				if((y == 100 || y == 200 || y == 300) && x >= 1500){
+					break;
+				}
+				if (y > 0) {
+				    y = parseInt(y) - parseInt(step);
+				}
 				break;
-			}
-			if(x===100 && ((y < 600) || (y >= 800))){
+			case 'a':
+				if(x == 1400 && y < 600){
+					break;
+				}
+				if(x == 200 && (y >= 500 && y < 600)){
+					break;
+				}
+				if (x > 0) {
+				    x = parseInt(x) - parseInt(step);
+				}
 				break;
-			}
-			if(x===1400 && (y < 100)){
+			case 's':
+				if((y == 400-step || y == 300-step || y == 200-step) && x >= 1500){
+					break;
+				}
+				if(y == 800-step && x >= 200 && x < 1500){
+					break;
+				}
+				if (y < 900-step) {
+				    y = parseInt(y) + parseInt(step);
+				}
 				break;
-			}
-			if(x===1500 && (y >= 100 && y < 500)){
+			case 'd':
+				if(x == 200-step && y >= 800){
+					break;
+				}
+				if(x == 400-step && (y >= 500 && y < 600)){
+					break;
+				}
+				if(x == 1500-step && ((y >= 400 && y < 800) || y < 100)){
+					break;
+				}
+				if (x < 1600-step) {
+				    x = parseInt(y) + parseInt(step);
+				}
 				break;
-			}
-			if(x===1300 && (y >= 500 && y < 600)){
-				break;
-			}
-		    if (x > 0) {
-		        x -= step;
-		    }
-		    break;
-		case 's':
-			if((x >= 100 && x < 200) && (y===100-step||y===200-step||y===300-step||y===400-step||y===600-step)){
-				break;
-			}
-			if(x < 100 && y === 800-step){
-				break;
-			}
-			if(x >= 1600 && (y===200-step||y===300-step||y===400-step)){
-				break;
-			}
-			if(((x >= 300 && x < 400) || (x >= 1400 && x < 1500)) && y === 100-step){
-				break;
-			}
-		    if (y < 900-step) {
-		        y += step;
-		    }
-		    break;
-		case 'd':
-			if((x===300-step || x===500-step || x===700-step || x===1100-step || x===1300-step || x===1500-step) && y >= 800 && y < 900){
-				break;
-			}
-			if(x===300-step&&y>=100&&y<500){
-				break;
-			}
-			if(x===1600-step&&y>=400&&y<600){
-				break;
-			}
-			if((x===400-step||x===1600-step)&&y<100){
-				break;
-			}
-			if(x===500-step&&(y>=500&&y<600)){
-				break;
-			}
-		    if (x < 1700-step) {
-		        x += step;
-		    }
-		    break;
-		default:
-		    // 如果按下的不是 WASD 键，则不做任何操作
-		    return;
+			default:
+				// 如果按下的不是 WASD 键，则不做任何操作
+				return;
+		}
 	}
-	
 	// 更新坐标显示
 	updateCoordinates();
 });
